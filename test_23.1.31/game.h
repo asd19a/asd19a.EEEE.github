45ceb56acb5fae7b3cd4fe1b608c4aa5c9026b5a
W_x_q123 <1839253204@qq.com>
2023-01-31 19:28:00 +0800

    1.31

---
+++ test_23.1.31/game.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define ROW 100
+#define COL 100
+
+char arr[ROW][COL];
+
---
+++ test_23.1.31/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include "game.h"
+
+
+void menu()
+{
+	printf("")
+}
+
+
+int main()
+{
+	int input = 0;
+	do
+	{
+		menu();
+		printf("ÇëÑ¡Ôñ:>");
+		scanf("%d", &input);
+		switch (input)
+		{
+		case 1:
+			break;
+
+		}
+	}
+}
