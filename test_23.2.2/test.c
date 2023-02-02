dc69395681ebe3595f0f7234a3c41403518d37a6
W_x_q123 <1839253204@qq.com>
2023-02-02 23:50:25 +0800

    2.2

--- test_23.2.2/test.c
+++ test_23.2.2/test.c
@@ -4,7 +4,50 @@
 #include <stdio.h>
 #include <string.h>
 
-int main()
-{
+//int main()
+//{
+//    int n = 5;
+//    int x = 1;
+//    int y = 0;
+//    int ret = 0;
+//    while (1)
+//    {
+//        n = 5;
+//        y = x / 5;
+//        while (n>1)
+//        {
+//            y = ((n - 1) * y )/ 5;
+//            if (y == 0 && n > 1);
+//            {
+//                goto end;
+//            }
+//            n--;
+//            
+//        }
+//        if (y%5==1)
+//        {
+//            printf("x=%d\n", x);
+//            break;
+//        }
+//    end:
+//        x++;
+//    }
+//}
 
-}
+
+//int main()
+//{
+//	int x = 1;
+//	int ret = 0;
+//	while (1)
+//	{
+//		ret = (24 * x - 33) % 5;
+//		if (ret == 1)
+//		{
+//			printf("%d\n", x);
+//			break;
+//		}
+//		x++;
+//	}
+//	return 0;
+//}
