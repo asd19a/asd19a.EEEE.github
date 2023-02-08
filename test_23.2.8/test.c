3bcd88c45dd6926c144e29c863e7ca067bf165e2
W_x_q123 <1839253204@qq.com>
2023-02-08 20:40:59 +0800

    2.8

---
+++ test_23.2.8/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//写一个宏，可以将一个整数的二进制位的奇数位和偶数位交换。
+
+//#define CHANGE(a,i) ((a)>>(i))
+//
+//int main()
+//{
+//	int a = 5;
+//	int i = 0;
+//	int tmp = 0;
+//	for (i = 0; i < 32; i=i+2)
+//	{
+//		
+//	}
+//}
+
+
+//int main()
+//{
+//	int a = 3;
+//	int      
+//}
