5a07b0472d307a4e2f63eadf39fa82d488697d2a
W_x_q123 <1839253204@qq.com>
2023-02-24 23:41:12 +0800

    2.24

--- test_23.2.24/test.c
+++ test_23.2.24/test.c
@@ -6,3 +6,30 @@
 #include <stdlib.h>
 #include <math.h>
 
+
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+	char arr[100];
+	gets(arr);
+	int digit = 0, space = 0;
+	int i = 0;
+	int len = strlen(arr);
+	for (i = 0; i < len; i++)
+	{
+		if (arr[i] >= '0' && arr[i] <= '9')
+		{
+			digit++;
+		}
+		else if (arr[i] == ' ')
+		{
+			space++;
+		}
+	}
+	printf("Êý×Ö:> %d\n", digit);
+	printf("×Ö·û:> %d\n", len);
+	printf("¿Õ¸ñ:> %d\n", space);
+	return 0;
+}
