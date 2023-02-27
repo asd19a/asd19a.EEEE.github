db47db54ed664edc3300d13b70e284aa56444c80
W_x_q123 <1839253204@qq.com>
2023-02-27 23:03:02 +0800

    单链表

--- test_23.2.26/test.c
+++ test_23.2.26/test.c
@@ -4,36 +4,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
+//int main()
+//{
+//	int arr1[3] = { 1,2,4 };
+//	int arr2[3] = { 1,3,4 };
+//	int arr3[6] = { 0 };
+//	int p1 = 0, p2 = 0;
+//	int k = 0;
+//	while (p1 < 3 && p2 < 3)
+//	{
+//		if (arr1[p1] < arr2[p2])
+//			arr3[k++] = arr1[p1++];
+//		else if (arr2[p2] < arr1[p1])
+//			arr3[k++] = arr2[p2++];
+//		else
+//		{
+//			arr3[k++] = arr1[p1++];
+//			arr3[k++] = arr2[p2++];
+//		}
+//	}
+//	while (p1 < 3)
+//	{
+//		arr3[k++] = arr1[p1++];
+//	}
+//	while (p2 < 3)
+//	{
+//		arr3[k++] = arr2[p2++];
+//	}
+//	for (int i = 0; i < k; i++)
+//	{
+//		printf("%d  ", arr3[i]);
+//	}
+//	return 0;
+//}
+
+
+struct t
 {
-	int arr1[3] = { 1,2,4 };
-	int arr2[3] = { 1,3,4 };
-	int arr3[6] = { 0 };
-	int p1 = 0, p2 = 0;
-	int k = 0;
-	while (p1 < 3 && p2 < 3)
-	{
-		if (arr1[p1] < arr2[p2])
-			arr3[k++] = arr1[p1++];
-		else if (arr2[p2] < arr1[p1])
-			arr3[k++] = arr2[p2++];
-		else
-		{
-			arr3[k++] = arr1[p1++];
-			arr3[k++] = arr2[p2++];
-		}
-	}
-	while (p1 < 3)
-	{
-		arr3[k++] = arr1[p1++];
-	}
-	while (p2 < 3)
-	{
-		arr3[k++] = arr2[p2++];
-	}
-	for (int i = 0; i < k; i++)
-	{
-		printf("%d  ", arr3[i]);
-	}
-	return 0;
+	int x;
+	struct t* y;
+}data[2] = { 10,0,20,0 };
+
+void main()
+{
+	struct t* p = data;
+	p->y = data;
+	printf("%d\n", ++p->x);
+
 }
