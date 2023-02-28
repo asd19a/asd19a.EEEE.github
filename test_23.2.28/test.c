cc66846acf50e60361d6700b7124c4705e5bdea8
W_x_q123 <1839253204@qq.com>
2023-02-28 16:16:57 +0800

    2.28

---
+++ test_23.2.28/test.c
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+#include <stdlib.h>
+
+//int fun(int a[], int n, int x)
+//{
+//	int* p = a;
+//	while (p < a + n && *p != x)
+//	{
+//		p++;
+//	}
+//	if (p < a + n)
+//		return p - a;
+//	else
+//		return -1;
+//}
+//
+//int main()
+//{
+//	int a[10] = { 1,2,3,4,5,6,7,8,9,10 };
+//	printf("%d\n", fun(a, 10, 10));
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	struct t
+//	{
+//		int x;
+//		int y;
+//	}a[2] = { 4,3,2,1 };
+//	printf("%d\n", a[0].y / a[0].x * a[1].x);
+//	return 0;
+//}
+
+int main()
+{
+	printf("%d\n", 3 / 4);
+	printf("%f\n", 3.0 / 4);
+	return 0;
+}
